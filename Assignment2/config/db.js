f63546c09836ed3999a71b7bb4657eb5a99f1c01
fdebugging <fatima_dabbous@hotmail.com>
2026-06-22 01:51:12 -0400

    Finalized Assignment 2

--- Assignment2/config/db.js
+++ Assignment2/config/db.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO URI:", process.env.MONGO_URI);
+    console.log("Attempting MongoDB connection...");
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Atlas Connected");
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.error("Mongo Error:");
+    console.error(err);
   }
 };
 
